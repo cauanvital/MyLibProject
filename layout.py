155546e6c2657382161411226c8fc3344808d086
cauanvital <cauanvital74@gmail.com>
2023-08-28 23:25:58 -0300

    Testing GUI

--- layout.py
+++ layout.py
@@ -1,9 +1,5 @@
 from BookClass import *
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
